ca4a1253a215d7de93fd93a80b9aaed7e8c9c350
Emilia <88168463+EmiliaTzy@users.noreply.github.com>
2021-10-12 13:57:42 +0200

    Update rm_download.py

--- handlers/rm_download.py
+++ handlers/rm_download.py
@@ -17,6 +17,6 @@ async def clear_downloads(_, message: Message):
     if ls_dir:
         for file in os.listdir(downloads):
             os.remove(os.path.join(downloads, file))
-        await message.reply_text("🗑️ **Delete all downloaded files**")
+        await message.reply_text("🗑️ **Menghapus semua file download**")
     else:
-        await message.reply_text("😕 **The downloaded file is empty, just like your heart!**")
+        await message.reply_text("😕 **File yang diunduh kosong, sama seperti hatimu!**")
