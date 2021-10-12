593cb8ae0139e3c1f04444afc801c5b0413d20a3
Emilia <88168463+EmiliaTzy@users.noreply.github.com>
2021-10-12 10:20:15 +0200

    Update broadcast.py

--- handlers/broadcast.py
+++ handlers/broadcast.py
@@ -17,17 +17,17 @@ async def broadcast(_, message: Message):
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`starting broadcast...`")
+        wtf = await message.reply("`memulai broadcast...`")
         if not message.reply_to_message:
-            await wtf.edit("please reply to a message to start broadcast!")
+            await wtf.edit("mohon balas ke pesan untuk broadcast!")
             return
         lmao = message.reply_to_message.text
         async for dialog in kennedy.iter_dialogs():
             try:
                 await kennedy.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`broadcasting...` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
+                await wtf.edit(f"`membroadcast...` \n\n**mengirim ke:** `{sent}` obrolan \n**failed in:** {failed} chats")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-        await message.reply_text(f"`gcast succesfully` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
+        await message.reply_text(f"`gcast berhasil` \n\n**mengirim ke:** `{sent}` obrolan \n**failed in:** {failed} chats")
