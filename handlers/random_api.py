f66c78789112c8b77c8d782957249a6d3cba1eda
Emilia <88168463+EmiliaTzy@users.noreply.github.com>
2021-10-12 15:40:56 +0200

    Update random_api.py

--- handlers/random_api.py
+++ handlers/random_api.py
@@ -6,25 +6,6 @@ from pyrogram import Client
 from config import BOT_USERNAME
 from helpers.filters import command
 
-@Client.on_message(command(["asupan", f"asupan@{BOT_USERNAME}"]))
-async def asupan(client, message):
-    try:
-        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
-        results = f"{resp['url']}"
-        return await client.send_video(message.chat.id, video=results)
-    except Exception:
-        await message.reply_text("`404 asupan videos not found:v`")
-
-
-@Client.on_message(command(["wibu", f"wibu@{BOT_USERNAME}"]))
-async def wibu(client, message):
-    try:
-        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
-        results = f"{resp['url']}"
-        return await client.send_video(message.chat.id, video=results)
-    except Exception:
-        await message.reply_text("`404 wibu not found:v`")
-
 
 @Client.on_message(command(["truth", f"truth@{BOT_USERNAME}"]))
 async def truth(client, message):
@@ -45,12 +26,3 @@ async def dare(client, message):
     except Exception:
         await message.reply_text("something went wrong...")
 
-
-@Client.on_message(command(["chika", f"chika@{BOT_USERNAME}"]))
-async def chika(client, message):
-    try:
-        resp = requests.get("https://api-tede.herokuapp.com/api/chika").json()
-        results = f"{resp['url']}"
-        return await client.send_video(message.chat.id, video=results)
-    except Exception:
-        await message.reply_text("`404 chika videos not found`")
